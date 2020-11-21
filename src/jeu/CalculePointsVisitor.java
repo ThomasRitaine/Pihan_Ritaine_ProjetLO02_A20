869c6f99a872bc615e06d568b05ae25d60b94e22
YaYa <yaelle.pihan@gmail.com>
2020-11-21 20:33:54 +0100

    Ok pb de version réglé je crois => calcul point dev à tester

--- src/jeu/CalculePointsVisitor.java
+++ src/jeu/CalculePointsVisitor.java
@@ -11,53 +11,18 @@ public class CalculePointsVisitor implements Visitor {
     private int calculerPoints(Carte carteVictoire, Plateau plateau) {
     	
     	this.setCarteVictoire(carteVictoire);
-<<<<<<< Updated upstream
-    	boolean estContinu;
-    	int score=0;
-    	int nbCaseLigneCouleur = 1;
-    	int compteurLigneForme = 0;
-    	int compteurLigneEstRemplie = 0;
-    	HashMap<Integer,Integer> NbPointsParLigne = new HashMap<Integer, Integer>();
-    	
-    	for(int y=0; y<=4; y++) {
-    		estContinu=false;
-    		nbCaseLigneCouleur =1;
-    		for(int x=0; x<=5; x++) {
-    			if(!plateau.getCase(x, y).estVide() && 
-    				plateau.getCase(x, y).getCarte().getCouleur().equals(carteVictoire.getCouleur())) {
-    				if(estContinu==false) {
-        				estContinu=true;
-        			}else {
-        				nbCaseLigneCouleur++;
-        				score+=nbCaseLigneCouleur-1;
-        			}
-    			}else {
-    				estContinu = false;
-    				nbCaseLigneCouleur =1;
-    			}
-    			
-    		}	
-    	}
-    		
-    	
-    	//	Test, on attribut des points aléatoires
-    	Random r = new Random();
-    	int max = 30;
-    	int min = 2;
-        return r.nextInt((max - min) + 1) + min;
-=======
-    	int scoreTotalManche = CalculerPointsLignesCouleur( carteVictoire, plateau) 
-							  +CalculerPointsLignesForme( carteVictoire, plateau)
-							  +CalculerPointsLignesRemplissage( carteVictoire, plateau);
+    	int scoreTotalManche = CalculerPointsLignesCouleur(plateau)
+				    		  +CalculerPointsLignesForme(plateau)
+				    		  +CalculerPointsLignesRemplissage(plateau);
     	return scoreTotalManche;
->>>>>>> Stashed changes
+
     }
 
     public void setCarteVictoire(Carte carteVictoire) {
         this.carteVictoire = carteVictoire;
     }
     
-	public int CalculerPointsLignesCouleur(Carte carteVictoire, Plateau plateau) {
+	public int CalculerPointsLignesCouleur(Plateau plateau) {
 		boolean estContinu;
 		int score = 0;
 		int nbCaseLigneCouleur = 1;
@@ -93,7 +58,7 @@ public class CalculePointsVisitor implements Visitor {
 		return score;
 	}
 	
-	public int CalculerPointsLignesForme(Carte carteVictoire, Plateau plateau) {
+	public int CalculerPointsLignesForme(Plateau plateau) {
 		boolean estContinu;
 		int score = 0;
 		int nbCaseLigneForme = 1;
@@ -131,7 +96,7 @@ public class CalculePointsVisitor implements Visitor {
 	}
 	
 	
-	public int CalculerPointsLignesRemplissage(Carte carteVictoire, Plateau plateau) {
+	public int CalculerPointsLignesRemplissage(Plateau plateau) {
 		boolean estContinu;
 		int score = 0;
 		int nbCaseLigneRemplissage = 1;
