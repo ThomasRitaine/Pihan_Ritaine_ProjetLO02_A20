a83a8685e1161e9eeec0ed432731767e46e8757c
YaYa <yaelle.pihan@gmail.com>
2021-01-22 15:21:26 +0100

    Javadoc générée + ajout package info exception + ajout DG classe et word etat

---
+++ src/fr/utt/sit/lo02/pihan_ritaine/shape_up/modele/exceptions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Ce package comprend les exceptions du modele.
+ * @author Yaëlle Pihan et Thomas Ritaine
+ * @version 1.0
+ */
+package fr.utt.sit.lo02.pihan_ritaine.shape_up.modele.exceptions;
